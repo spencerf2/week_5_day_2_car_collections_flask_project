6a0f17d4ba2aaef2863026d1d75d2bc0bd738fb3
spencerf2 <spencer.franklin@hotmail.com>
2021-09-01 02:23:21 +0200

    Second Commit. Added database functionality. Added a form to signup.

--- car_collection/__init__.py
+++ car_collection/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from config import Config
 from .site.routes import site
 from .authentication.routes import auth
+from .models import db as root_db
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
@@ -9,3 +12,6 @@ app.register_blueprint(site)
 app.register_blueprint(auth)
 
 app.config.from_object(Config)
+
+root_db.init_app(app)
+migrate = Migrate(app, root_db)
--- config.py
+++ config.py
@@ -13,6 +13,7 @@ class Config():
     """
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:enter_password_here@127.0.0.1:5432/car-collection'
     SQLALCHEMY_TRACK_MODIFICATIONS = False # Turn off Update Messages from sqlalchemy
     
